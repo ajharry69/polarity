02c4a69118b57626c07e26be5125d240d21c92f7
Orinda Harrison <ajuongharry35@gmail.com>
2020-05-15 00:09:49 +0300

    #3 template render from loader

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import *
+
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    # dictionary mapping template variable names to Python objects
+    context = {
+        'latest_questions': latest_questions
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}')
