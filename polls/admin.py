2ad84f6d3e49c0b9b58b011bfa6ff57384dbecf7
Orinda Harrison <ajuongharry35@gmail.com>
2020-05-15 19:36:33 +0300

    #7 modify admin intro

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  # provide enough fields for 3 choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # field names to display, as columns, on the change list page for the object
+    list_display = ('question_text', 'pub_date', 'was_published_recently',)
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {
+            'fields': ['pub_date'],
+            'classes': ['collapse'],
+        })
+    ]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
